21b29d35269a0096d230471ad9274e5af703c082
Mikael Abrahamsson <m64253@users.noreply.github.com>
2016-08-31 18:17:13 +0200

    Update rules.js

--- test/rules.js
+++ test/rules.js
@@ -1,14 +1,15 @@
-/* eslint func-names: 1, prefer-arrow-callback: 1, no-path-concat: 1 */
+/* eslint func-names: 1, prefer-arrow-callback: 0, no-path-concat: 1 */
 /* globals describe:true, it:true */
 var assert = require('assert');
 var fs = require('fs');
+var path = require('path');
 var eslint = require('eslint');
 
 
-describe('Base config no deprecations', function () {
-  it('should return empty messages', function () {
-    var config = JSON.parse(fs.readFileSync(__dirname + '/../.eslintrc'));
-    var messages = eslint.linter.verify('', config, { filename: 'foo.js' });
+describe('Base config no deprecations', function testSuite() {
+  it('should return empty messages', function testCase() {
+    var config = JSON.parse(fs.readFileSync(path.join(__dirname, '/../.eslintrc')));
+    var messages = eslint.linter.verify('', config, { filename : 'foo.js' });
 
     assert.deepEqual(messages, []);
   });
