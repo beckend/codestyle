f443701488c439abf0404d8aa8594af22b68a625
Enzo Martin <EnzoMartin@users.noreply.github.com>
2016-09-20 13:23:05 +0200

    Enforce comma spacing (#44)
    * Enforce comma spacing
    
    * Fix lint

--- test/rules.js
+++ test/rules.js
@@ -6,11 +6,11 @@ const fs = require('fs');
 
 describe('Base config no deprecations', () => {
   it('should return empty messages', () => {
-    fs.readFile(path.join(__dirname,'../.eslintrc'),(err,data) => {
+    fs.readFile(path.join(__dirname, '../.eslintrc'), (err, data) => {
       const config = JSON.parse(data);
       const messages = eslint.linter.verify('', config, { filename : 'foo.js' });
 
-      assert.equal(err,null);
+      assert.equal(err, null);
       assert.deepEqual(messages, []);
     });
   });
